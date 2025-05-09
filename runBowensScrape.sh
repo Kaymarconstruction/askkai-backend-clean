8faa41c7cbcdea60ce3cd327ac54e90cabdc5ec1
Kaymarconstruction <mark@kaymarconstruction.com>
2025-05-10 09:54:26 +1000

    Update runBowensScrape.sh

--- runBowensScrape.sh
+++ runBowensScrape.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Configuration
-BACKEND_URL="https://askkai-backend-clean.onrender.com"
-EMAIL="mark@kaymarconstruction.com"
-
-echo -e "\nStarting Bowens Scraper..."
-curl -s -X POST "$BACKEND_URL/scrape/bowens" \
-    -H "Content-Type: application/json" \
-    -d "{\"email\": \"$EMAIL\"}" | jq
-
-echo -e "\nScraping Completed!"
+##!/bin/bash
+echo "Running Bowens Scraper..."
+npm run scrape:bowens
