850e057001c39f3681f81586af92054fb0abbe27
Kaymarconstruction <mark@kaymarconstruction.com>
2025-05-17 17:17:24 +1000

    Update askkai.js

--- askkai.js
+++ askkai.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
 const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 10000;
